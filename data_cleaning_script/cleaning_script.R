729dddee956ff964572a3d02b779fec4c7c48677
Hoodd82 <87151109+Hoodd82@users.noreply.github.com>
2021-10-13 11:39:02 +0100

    cleaning script functions update

--- data_cleaning_script/cleaning_script.R
+++ data_cleaning_script/cleaning_script.R
@@ -4,9 +4,7 @@ pacman::p_load(
   here,        
   janitor,    
   lubridate,  
-  tidyverse,
-  skimr,
-  hms
+  tidyverse
 )
 
 # read_in_datasets --------------------------------------------------------
@@ -20,79 +18,77 @@ weather <- read_csv(here("raw_data/weather.csv"))
 # data_cleaning -----------------------------------------------------------
 
 weather <- weather %>% 
-  select(-c(temp, dewp, humid, precip, pressure, year, month, day, hour)) # drop variables where proportion of NAs is greater than 90%
+  select(-c(temp, dewp, humid, precip, pressure, 
+            year, month, day, hour)) # drop variables where proportion of NAs is greater than 90%
 
 airports <- airports %>% 
-  select(-c(tz, dst, tzone)) # drop variables which will not be used in the final analysis
+  select(-c(tz, dst, tzone)) # to avoid irregularities due to changes in timezones, New York local time will be the reference
 
 aircraft <- aircraft %>%
-  mutate(year = na_if(year, 0)) %>% 
+  mutate(year = na_if(year, 0)) %>% # avoid issues with aircraft with year equal zero
   mutate(aircraft_age = as.numeric(format(Sys.Date(), "%Y")) - year) %>% # create aircraft age variable
   select(-c(engines, speed, year, type, engine)) # drop variables which will not be used in the final analysis
 
-flights <- flights %>%
+datetime_fn <- function(year, month, day, time) {
+  make_datetime(year, month, day, time %/% 100, time %% 100)
+} # using modulus arithmetic we can pull the hour and minutes from the departure and arrival variables
+
+flights <- flights %>% 
+  mutate(
+    dep_time = datetime_fn(year, month, day, dep_time),
+    arr_time = datetime_fn(year, month, day, arr_time),
+    sched_dep_time = datetime_fn(year, month, day, sched_dep_time),
+    sched_arr_time = datetime_fn(year, month, day, sched_arr_time)
+  ) %>% # use the datetime_fn to create new departure and arrival variables
   drop_na() %>% # drop NAs since these observations will not be useful in the analysis
-  mutate(depart_date = str_c(year, month, day, sep = "-")) %>% # create departure date
-  mutate(across(.cols = c(dep_time, sched_dep_time, arr_time, sched_arr_time), 
-                .fns = ~ str_pad(.x, width = 4, pad = 0))) %>% # convert the time variables to hhmm
-  mutate(across(.cols = c(dep_time, sched_dep_time, arr_time, sched_arr_time), 
-                .fns = ~ str_replace(.x, "^(..)", "\\1:"))) %>% # convert to hh:mm
-  mutate(sched_depart_datetime = ymd_hm(paste(depart_date, sched_dep_time))) %>% # create scheduled departure datetime column
-  mutate(act_depart_datetime = sched_depart_datetime + dep_delay * 60) %>% # create actual departure date column by adding dep_delay
   mutate(origin_name = case_when(
     origin == "EWR" ~ "Newark Liberty Int. Airport",
     origin == "JFK" ~ "John F. Kennedy Int. Airport",
     origin == "LGA" ~ "LaGuardia Airport"
-  ), .after = "origin") %>% # create origin_name variable for airport names
-  select(-c(dep_time, sched_dep_time, arr_time, sched_arr_time, arr_delay, depart_date, year, month, day, hour, minute))  # drop variables which are no longer required
-
-# the brief is to investigate departure delays therefore variables relating to arrivals have been dropped - the assumption is that an arrival delay is as a result of a departure delay which falls outwith the scope of the project
-# destination information is retain as there may be useful insights around whether some routes suffer from delays more than others
+  ), .after = "origin") %>% # create origin_name for airport names, easier to do this rather than performing an additional join
+  select(-c(year, month, day, hour, minute))  # drop variables which are no longer required
 
 # first_join --------------------------------------------------------------
 
-flights_joined <- flights %>% 
-  left_join(airlines, by = "carrier") %>% 
-  left_join(aircraft, by = "tailnum") %>% 
-  left_join(airports, by = c("dest" = "faa"))
+flights <- flights %>% 
+  left_join(airlines, by = "carrier") %>% # join with airline dataset
+  left_join(aircraft, by = "tailnum") %>% # join with aircraft dataset
+  left_join(airports, by = c("dest" = "faa")) # join with the airport dataset
 
-# subset_datatsets_by_airport ---------------------------------------------
+# flights_weather_join ----------------------------------------------------
 
-# create individual datasets from flights and weather to ensure the correct weather information is joined to the correct airport data, this avoids any conflicts where flights have departed at the same time
+# the following function takes an input of the flights and weather datasets, then subsets them based on origin (this is performed to avoid any conflicts which might arise when a flight departs at the same time_hour from different airports when weather is joined with flights), joins the datasets and combines them using bind_rows
 
-ewr_flights <- flights_joined %>% 
-  filter(origin == "EWR")
-jfk_flights <- flights_joined %>% 
-  filter(origin == "JFK")
-lga_flights <- flights_joined %>% 
-  filter(origin == "LGA")
+flights_weather_join_fn <- function(dataset1, dataset2){
+  # subset flights
+  ewr_flights <- dataset1 %>% 
+    filter(origin == "EWR")
+  jfk_flights <- dataset1 %>% 
+    filter(origin == "JFK")
+  lga_flights <- dataset1 %>% 
+    filter(origin == "LGA")
+  # subset weather
+  ewr_weather <- dataset2 %>% 
+    filter(origin == "EWR")
+  jfk_weather <- dataset2 %>% 
+    filter(origin == "JFK")
+  lga_weather <- dataset2 %>% 
+    filter(origin == "LGA")
+  # perform the joins
+  ewr_flights_joined <- ewr_flights %>% 
+    left_join(ewr_weather, by = "time_hour")
+  jfk_flights_joined <- jfk_flights %>% 
+    left_join(jfk_weather, by = "time_hour")
+  lga_flights_joined <- lga_flights %>% 
+    left_join(lga_weather, by = "time_hour")
+  # combine the resulting datasets
+  flights_joined <- bind_rows(
+    ewr_flights_joined, jfk_flights_joined, lga_flights_joined)
+  # assign the output to flights
+  flights <<- flights
+}
 
-ewr_weather <- weather %>% 
-  filter(origin == "EWR")
-jfk_weather <- weather %>% 
-  filter(origin == "JFK")
-lga_weather <- weather %>% 
-  filter(origin == "LGA")
-
-# second_joins ------------------------------------------------------------
-
-# join the subset datasets
-
-ewr_flights_joined <- ewr_flights %>% 
-  left_join(ewr_weather, by = "time_hour")
-ewr_flights_joined
-
-jfk_flights_joined <- jfk_flights %>% 
-  left_join(jfk_weather, by = "time_hour")
-jfk_flights_joined
-
-lga_flights_joined <- lga_flights %>% 
-  left_join(lga_weather, by = "time_hour")
-lga_flights_joined
-
-# bind_datasets -----------------------------------------------------------
-
-flights_joined <- bind_rows(ewr_flights_joined, jfk_flights_joined, lga_flights_joined)
+flights_weather_join_fn(flights, weather)
 
 # final_clean -------------------------------------------------------------
 
